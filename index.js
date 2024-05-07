a177d6c57f2fb6d28ae66dde5a89e1e132cdf97c
uw7tyguywe <gs1abel5329@hotmail.com>
2024-05-08 00:19:52 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-function longestConsecutive(nums) {
-  const set = new Set(nums);
-  let longest = 0;
-  for (const num of set) {
-    if (!set.has(num - 1)) {
-      let currentNum = num;
-      let currentStreak = 1;
-      while (set.has(currentNum + 1)) {
-        currentNum++;
-        currentStreak++;
-      }
-      longest = Math.max(longest, currentStreak);
-    }
-  }
-  return longest;
-}
+const factorial = (num) => {
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
+};
