e69cfafc460dbebcaf20e1f50d4eae3705cd7243
uw7tyguywe <gs1abel5329@hotmail.com>
2024-05-11 21:22:46 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const isEven = (num) => num % 2 === 0;
+function sortColors(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  let i = 0;
+  while (i <= right) {
+    if (nums[i] === 0) {
+      [nums[i], nums[left]] = [nums[left], nums[i]];
+      left++;
+      i++;
+    } else if (nums[i] === 2) {
+      [nums[i], nums[right]] = [nums[right], nums[i]];
+      right--;
+    } else {
+      i++;
+    }
+  }
+}
