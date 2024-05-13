09748cd9576d109d9302f2cdff4e7eecfbf2a62e
uw7tyguywe <gs1abel5329@hotmail.com>
2024-05-13 22:00:45 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const pullAtIndex = (arr, pullArr) => {
-  let removed = [];
-  let pulled = arr
-    .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
-    .filter((v, i) => !pullArr.includes(i));
-  arr.length = 0;
-  pulled.forEach((v) => arr.push(v));
-  return removed;
+const lcmRecursive = (a, b) => {
+  return (a * b) / gcdRecursive(a, b);
 };
